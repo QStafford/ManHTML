7906da539331b1437c1eb4d4b8273ee648a0777f
Quincy Stafford <xXQS316Xx@gmail.com>
2021-09-23 09:56:08 -0500

    added DOMcontent Loader and reset

--- DOM-version/app.js
+++ DOM-version/app.js
@@ -1,5 +1,9 @@
-let button = document.createElement('button')
-let btnText = document.createTextNode('Click Here!')
+document.addEventListener("DOMContentLoaded", function () {
+    let button = document.createElement('button')
+    let btnText = document.createTextNode('Click Here!')
 
-button.appendChild(btnText)
-document.body.appendChild(button)
+    button.appendChild(btnText)
+    document.body.appendChild(button)
+
+})
+
---
+++ jQuery-version/app.js
@@ -0,0 +1,1 @@
+$('.button').before('.body')
