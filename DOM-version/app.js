b113fd16e553abbd91aee7a1d38f7f29330d9162
Quincy Stafford <xXQS316Xx@gmail.com>
2021-09-23 13:21:03 -0500

    added div, mouse colors

--- DOM-version/app.js
+++ DOM-version/app.js
@@ -8,10 +8,32 @@ document.addEventListener("DOMContentLoaded", function () {
     button.addEventListener("click", function () {
         window.alert("1. Hello! How are you doing this fine evening?!?!?!");
     })
+
+    let colorchange = document.createElement("div")
+
+    document.body.appendChild(colorchange)
+    
+    colorchange.style.backgroundColor = 'pink'
+    colorchange.style.borderColor = 'black'
+    colorchange.style.border = 'solid' 
+    colorchange.style.width = '100px'
+    colorchange.style.height = '100px'
+
+    colorchange.addEventListener('mouseenter', function () {
+        colorchange.style.backgroundColor = 'purple'
+    })
+    colorchange.addEventListener('mouseleave', function () {
+        colorchange.style.backgroundColor = 'pink'
+    })
+
+
+
 })
 
 function feelsalrt() {
-    if(confirm("2. Do you want to update message??")){
+    if (confirm("2. Do you want to update message??")) {
         alert(document.getElementById("feelsbox").value)
     }
 }
+
+
